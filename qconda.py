caf310a032141041dc7bf8e8f4982103cb4ea2b4
John Reeves <j.reeves@garvan.org.au>
2024-09-22 12:51:12 +1000

    Introduce sub-commands

--- qconda.py
+++ qconda.py
@@ -1,44 +1,46 @@
-"""
-Provides tools for running qsub jobs on PBS Pro in particular environments. 
-This reduces boilerplate code for activating environments and switching directories.
-In simple cases, the need to create a jobscript can be eliminated entirely.
-"""
-
-import os
 import click
 import pkg_resources
+import os
 
-@click.command()
+@click.group()
+@click.option("-l", "--resources", multiple=True, help="Job resource")
+@click.option("-q", "--queue", default="normal", help="Job queue")
+@click.option("-N", "--name", default="qconda", help="Job name")
+@click.option("-P", "--project", default="a56", help="Project")
+@click.option("-v", "--verbose", is_flag=True, default=False, help="Verbose output")
+@click.pass_context
+def qt(ctx, resources, queue, name, project, verbose):
+    """Parent command 'qt' with shared options."""
+    ctx.ensure_object(dict)
+    # Construct the submission command
+    options = f"-N {name} -q {queue} -P {project} "
+    for resource in resources:
+        options += f"-l {resource} "
+    if verbose:
+        click.echo(f"Options: {options}")
+    ctx.obj['options'] = options
+    ctx.obj['verbose'] = verbose
+
+@qt.command()
 @click.argument("cmd", nargs=-1)
 @click.option("--env", required=True, help="Conda environment to use")
-@click.option("-l","--resources", multiple=True, help="Job resource")
-@click.option("-q","--queue", default="normal", help="Job queue")
-@click.option("-N","--name", default="qconda", help="Job name")
-@click.option("-P","--project", default="a56", help="Project")
-@click.option("-v","--verbose", is_flag=True, default=False, help="Verbose output")
-def conda_submit(cmd, env, resources, queue, name, project, verbose):
-    """Runs a command/script in a given environment with the specified PBS options"""
-    if verbose:# Display the received inputs
+@click.pass_context
+def conda(ctx, cmd, env):
+    """Runs a command in a given environment."""
+    options = ctx.obj['options']
+    verbose = ctx.obj['verbose']
+
+    if verbose:
         click.echo(f"Conda environment: {env}")
-        click.echo(f"Resources: {resources}")
-        click.echo(f"Queue: {queue}")
-        click.echo(f"Name: {name}")
-        click.echo(f"Project: {project}")
         click.echo(f'Command: {cmd}')
 
     # Get jobscript path
     jobscript = pkg_resources.resource_filename(__name__, 'jobscripts/qconda.pbs')
-    # Get current work directory
-    cwd=os.getcwd()
-
-    # Construct the submission command
-    full_options=f"-N {name} " + f"-q {queue} " + f"-P {project} "
-    for resource in resources:
-        full_options+=f"-l {resource}"
-    if verbose:
-        click.echo(f"Options: {full_options}")
-    full_cmd=" ".join(cmd)
-    submission_command = f'qsub -v env={env},cmd="{full_cmd}",cwd={cwd} {full_options} {jobscript}'
+    # Get current working directory
+    cwd = os.getcwd()
+    
+    full_cmd = " ".join(cmd)
+    submission_command = f'qsub -v env={env},cmd="{full_cmd}",cwd={cwd} {options} {jobscript}'
 
     # Execute the command
     click.echo(f'Submitting: {submission_command}')
--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ from setuptools import setup
 setup(
     name="qconda",
     version="0.1",
-    py_modules=["qconda"],  # Name of the Python file without the extension
+    py_modules=["qconda"],
     install_requires=["click"],
     package_data={
         '': ['jobscripts/qconda.pbs'],
     },
     entry_points={
         'console_scripts': [
-            'qconda = qconda:conda_submit',
+            'qt = qconda:qt',  # Register 'qt' as the main command
         ],
     },
 )
