a1e51e7903fa558a0b89b459c1d6a3b540d28e0b
John Reeves <j.reeves@garvan.org.au>
2025-10-27 08:11:04 +1100

    Bump version in setup.py to 3.2.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="qxub",
-    version="3.2.0",
+    version="3.2.3",
     author="John Reeves",
     author_email="j.reeves@garvan.org.au",
     description="Simplified job submission to HPC",
